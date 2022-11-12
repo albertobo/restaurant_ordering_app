2d999aeb4c9242e3388dc8cc5738951736b9e32c
albertobo <117069548+albertobo@users.noreply.github.com>
2022-11-12 14:07:00 +0100

    Initial commit

---
+++ restaurant_ordering_app.js
@@ -0,0 +1,124 @@
+//import {v4 as uuidv4} from './uuid'
+import {menuArray} from "./data.js" 
+
+const menu = document.getElementById("menu")
+const orderList = document.getElementById("order-list")
+const form = document.getElementById("hidden-form")
+let orderListArray = []
+let htmlOrderList = ""
+let id = 0
+
+menu.innerHTML = getHtml()
+
+document.addEventListener("click", function(e) {
+    if(e.target.dataset.id){
+        for(let element of document.getElementsByClassName("hidden")){
+            console.log(element)
+            element.classList.remove("hidden")
+        }
+        menuArray.forEach(function(element) {
+            if(element.id == e.target.dataset.id){
+                const orderElement = {
+                    name: element.name,
+                    price: element.price,
+                    button: "remove",
+                    id: id
+                }
+                id++
+                orderListArray.push(orderElement)               
+            }
+            })
+            orderListArray.forEach(function(element) {
+                htmlOrderList = ` <div class="order-element-container">
+                                  <h3>${element.name}</h3>
+                                  <button class="remove-btn" data-order="${element.id}">${element.button}</button>
+                                  <h3>${element.price}</h3>
+                                  </div>
+                                  `
+            })
+            orderList.innerHTML += htmlOrderList         
+        
+    }
+     if(e.target.dataset.order){
+        orderListArray.forEach(function(element) {
+            if(e.target.dataset.order == element.id){
+                if(orderListArray.indexOf(element) != 0){
+                let a = orderListArray.splice(orderListArray.indexOf(element), orderListArray.indexOf(element))
+                orderListArray.push(a)
+                orderListArray.pop()
+                }
+                else if(orderListArray.indexOf(element) == 0) {
+                    orderListArray.shift()
+                    console.log(orderListArray.indexOf(element))
+                    console.log(orderListArray)
+                }
+            }
+            
+        })
+            if(orderListArray.length === 0){
+                htmlOrderList = ""
+                orderList.innerHTML = htmlOrderList
+            }
+            else {
+                  htmlOrderList = ""
+                  orderListArray.forEach(function(element) {
+                  htmlOrderList += `<div class="order-element-container">
+                                   <h3>${element.name}</h3>
+                                   <button class="remove-btn" data-order="${element.id}">${element.button}</button>
+                                   <h3>${element.price}</h3>
+                                   </div>
+                                   `
+                  })
+                  orderList.innerHTML = htmlOrderList
+              }
+            
+    }
+    
+    if(e.target.dataset.complete) {
+        form.style.display = "block"
+        form.classList.toggle("form")
+    }
+    
+    if(e.target.dataset.pay) {
+        form.style.display = "none"
+        const name = document.getElementById("name").value
+        const deleteItem = document.getElementById("delete")
+        deleteItem.innerHtML = `<h3 class="center">Thanks ${name}, your order is on its way!</h3>`
+        deleteItem.innerHTML = `
+                                <div class="center">
+                                <h3>Thanks ${name}, your order is on its way!</h3>
+                                </div>`        
+    }
+})
+
+function getHtml(){
+let htmlStr = ""
+for(let item in menuArray){
+  htmlStr += `<div class="item">
+              <p class="emoji">
+               ${menuArray[item].emoji}
+                <p>
+                 <div class="menu-text">
+                 <h3 class="name">
+                  ${menuArray[item].name}
+                   </h3>
+                    <p class="ingredients">
+                     ${menuArray[item].ingredients}
+                      </p>
+                       <p class="price">
+                        ${menuArray[item].price + "$"} 
+                         </p>
+                          </div>
+                           <button class="add-btn" data-id="${menuArray[item].id}">
+                            +
+                            </button>
+                             </div>
+                             `
+                  
+}
+return htmlStr
+}
+
+
+
+
